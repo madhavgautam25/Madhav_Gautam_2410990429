f27633252e769bbb664b6fb776fdd70fa4e612d7
Madhav Gautam <gautam.madhav63@gmail.com>
2025-07-03 10:11:04 +0530

    Added a c++ file including product of two numbers after taking inputs from the user.

---
+++ program2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter 1st number:";
+    cin>>x;
+    cout<<"Enter 2nd number:";
+    cin>>y;
+    cout<<"Product is: "<<(x*y);
+    return 0;
+}
